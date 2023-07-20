d0bd23eb2716ca365787369eacc7bd31f205f30d
oDANKFACE <darien.r.weller@gmail.com>
2023-07-20 15:13:23 -0600

    OAuth is working locally, fingers crossed

--- app.js
+++ app.js
@@ -2,40 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes');
 const mongodb = require('./mongo');
+const session = require('express-session');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const app = express();
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use('/', router);
-
-process.on('uncaughtException', (error, origin) => {
-    console.log(process.stderr.fd, `Error thrown: ${error} with origin: ${origin}`);
-});
-
-
-app.listen(port, () => {
-    console.log('Web Server is listening at port ' + port);
-});
-
-mongodb.initDb((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Connected to db');
-    }
-});
-
-
-const { auth, requiresAuth } = require('express-openid-connect');
+const {auth, requiresAuth} = require('express-openid-connect');
 const config = {
     authRequired: false,
     auth0Logout: true,
@@ -44,6 +21,12 @@ const config = {
     clientID: process.env.CLIENT_ID,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use(auth(config));
 
@@ -55,3 +38,20 @@ app.get('/profile', requiresAuth(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', router);
+
+process.on('uncaughtException', (error, origin) => {
+    console.log(process.stderr.fd, `Error thrown: ${error} with origin: ${origin}`);
+});
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log('Web Server is listening at port ' + port);
+            console.log('Connected to db');
+        });
+    }
+});
